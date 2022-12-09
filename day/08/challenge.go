c73f0ab17caad4841d48c3dbd45a1a17e9d8b89f
John Roberts <john.calvin.roberts@gmail.com>
2022-12-09 09:29:20 -0600

    refactor day 8

--- day/08/challenge.go
+++ day/08/challenge.go
@@ -6,87 +6,92 @@ import (
 	"github.com/johncalvinroberts/advent-of-code-2022/utils"
 )
 
-type ForestTree struct {
-	height  int
-	visible bool
+type Grid [][]int
+
+type Forest struct {
+	grid          Grid
+	height, width int
 }
 
 // used for converting rune to int
 const zero rune = '0'
 
+// gets visible trees from all 4 directions
+func (g *Forest) countVisible() int {
+	// set of any visible trees with key "rowcol"
+	visible := utils.Set[string]{}
+
+	check_height := func(r, c, max int) int {
+		tree_height := g.grid[r][c]
+		if tree_height > max {
+			// tree is > max therefore visible
+			visible.Add(fmt.Sprint(r, c))
+			return tree_height
+		}
+		return max
+	}
+
+	// start now on inner trees HORIZONTAL
+	for r := 1; r < g.height-1; r++ {
+		// keep track of max so far
+		// L->R (break if max == 9)
+		max := g.grid[r][0]
+		for c := 1; c < g.width-1 && max < 9; c++ {
+			max = check_height(r, c, max)
+		}
+
+		// R->L (break if max == 9)
+		max = g.grid[r][g.width-1]
+		for c := g.width - 2; c > 0 && max < 9; c-- {
+			max = check_height(r, c, max)
+		}
+	}
+
+	// VERTICAL
+	for c := 1; c < g.width-1; c++ {
+		// keep track of max so far
+		// T->B (break if max == 9)
+		max := g.grid[0][c]
+		for r := 1; r < g.height-1 && max < 9; r++ {
+			max = check_height(r, c, max)
+		}
+
+		// B->T (break if max == 9)
+		max = g.grid[g.height-1][c]
+		for r := g.height - 2; r > 0 && max < 9; r-- {
+			max = check_height(r, c, max)
+		}
+	}
+
+	// include edges (top, bottom, left, right)
+	edges := g.height*2 + (g.width-2)*2
+
+	return len(visible) + edges
+}
+
 // find how many trees are visible from outside the grid
 func Part1(input string) int {
-	rows := utils.StrToSlice(input, "\n")
 	var (
+		rows   = utils.StrToSlice(input, "\n")
 		width  = len(rows[0])
 		height = len(rows)
+		grid   = make(Grid, height)
+		trees  = &Forest{
+			height: height,
+			width:  width,
+		}
 	)
 
-	// build set of trees
-	// key = string, representing row and column
-	allTreesVisibility := make(map[string]*ForestTree)
-	for y, row := range rows {
-		for x, tree := range row {
-			var (
-				nodeHeight = int(tree - zero)
-				visible    = false
-			)
-			if x == 0 || y == 0 || x == width-1 || y == height-1 {
-				visible = true
-			}
-			entry := &ForestTree{height: nodeHeight, visible: visible}
-			allTreesVisibility[getKey(x, y)] = entry
+	for r, line := range rows {
+		grid[r] = make([]int, width)
+		for c, char := range line {
+			grid[r][c] = int(char - zero)
 		}
 	}
-	// horizontal
-	for r := 1; r < height-1; r++ {
-		leftMax := int(rows[r][0])
-		rightMax := int(rows[r][width-1])
-		for c := 1; c < width-1; c++ {
-			fmt.Printf("left: %v, right: %v\n", getKey(r, c), getKey(r, width-1-c))
-			leftCurrent := int(rows[r][c])
-			rightCurrent := int(rows[r][width-1-c])
-			if leftCurrent > leftMax {
-				leftMax = leftCurrent
-				allTreesVisibility[getKey(r, c)].visible = true
-			}
-			if rightCurrent > rightMax {
-				rightMax = rightCurrent
-				allTreesVisibility[getKey(r, width-1-c)].visible = true
-			}
-		}
-	}
-	// vertical
-	for c := 1; c < width-1; c++ {
-		topMax := int(rows[0][c])
-		bottomMax := int(rows[height-1][c])
-		for r := 1; r < height-1; r++ {
-			topCurrent := int(rows[r][c])
-			bottomCurrent := int(rows[height-1-r][c])
-			if topCurrent > topMax {
-				topMax = topCurrent
-				allTreesVisibility[getKey(r, c)].visible = true
-			}
-			if bottomCurrent > bottomMax {
-				bottomCurrent = bottomMax
-				allTreesVisibility[getKey(height-1-r, c)].visible = true
-			}
-		}
-	}
-	var count int
-	for _, v := range allTreesVisibility {
-		if v.visible {
-			// fmt.Printf("k: %v, v: %v\n", k, v.height)
-			count++
-		}
-	}
-	return count
+	trees.grid = grid
+	return trees.countVisible()
 }
 
 // guessed: 7581, too high
 
 func Part2(input string) {}
-
-func getKey(x, y int) string {
-	return fmt.Sprintf("%d-%d", x, y)
-}
---
+++ utils/set.go
@@ -0,0 +1,19 @@
+package utils
+
+type Set[T comparable] map[T]struct{}
+
+func (set *Set[T]) Add(items ...T) {
+	for _, item := range items {
+		(*set)[item] = struct{}{}
+	}
+}
+
+func (set *Set[T]) Delete(item T) {
+	delete(*set, item)
+}
+
+func (set *Set[T]) Has(item T) bool {
+	_, ok := (*set)[item]
+
+	return ok
+}
